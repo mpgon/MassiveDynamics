4f5f0264c0ecc463b78345e3a4a4372af4b7bc0e
Marco Garcia <marcoamgarcia17@gmail.com>
2014-01-05 22:41:02 +0000

    1101012: Criacao dos predicados para o pc realizar a melhor jogada no caso de nao poder ganhar imediatamente nem precise de cortar jogada adversaria. #7

--- Prolog/MassiveDynamicsGalo/jogo_do_galo.pl
+++ Prolog/MassiveDynamicsGalo/jogo_do_galo.pl
@@ -36,7 +36,7 @@ vitoriao([5,4,3]).
 vitoriao([8,7,6]).
 
 %inicio
-inicio(P,J):-verPcWin(P);counter(J);write('vai jogar com prob').
+inicio(P,J):-verPcWin(P);counter(J);melhorjogada(P,J).
 
 subset([],_,N,N).
 subset([H|T],List,N,Listax):-member(H,List),N1 is N + 1,subset(T,List, N1,Listax).
@@ -97,14 +97,15 @@ pcWin3([H|T],[H2|T2]):-H \== H2,
 pcWin3([H|T],[H|B]):-pcWin3(T,B).
 
 
-
+% vai dar counter!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 counter(J):-sort(J, L1),
 	findall(A,(vitoriax(A)),Lv),
 	pesquisa2(L1,Lv,_).
 
-
 subsetCounter([],_,N,N).
+
 subsetCounter([H|T],List,N,Listax):-member(H,List),N1 is N + 1,subsetCounter(T,List, N1,Listax).
+
 subsetCounter([H|T],List,N,Listax):- \+member(H,List),!,subsetCounter(T,List,N,Listax).
 
 pesquisa2(L1,[],Listax):-
@@ -156,6 +157,99 @@ pcCounter3([H|T],[H2|T2]):-H \== H2,
 	pcCounter3(T,[H2|T2]).
 
 pcCounter3([H|T],[H|B]):-pcCounter3(T,B).
+
+
+%vai jogar na melhor prob!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+melhorjogada(P,J):-sort(P, L1),
+	sort(J,L2),
+	findall(A,(vitoriax(A)),Lv),
+	pesquisa3(L1,L2,Lv,_,_).
+
+subsetmj([],_,N,N).
+
+subsetmj([H|T],List,N,Listax):-member(H,List),N1 is N + 1,subsetmj(T,List, N1,Listax).
+
+subsetmj([H|T],List,N,Listax):- \+member(H,List),!,subsetmj(T,List,N,Listax).
+
+pesquisa3(L1,L2,[],Listax,Listax2):-
+	reverse(Listax, Lx),
+	reverse(Listax2,Lx2),
+	%print(Lx),
+	nl,
+	%print(L1),
+	!,
+	testarmj(L1,L2,Lx,Lx2,1).
+
+pesquisa3(L1,L2,[H|T],Listax,Listax2):-subsetmj(L1,H,0,HListax),
+	subsetmj(L2,H,0,HListax2),
+	%print(Listax),nl,
+	%pcWin(L1,Listax,0),
+	(pesquisa3(L1,L2,T,[HListax|Listax],[HListax2|Listax2])).
+
+testarmj(L1,L2,Lx,[1|T2],N):-subs(N,Lx,Rlx),
+	N1 is N + 1,
+	testarmj(L1,L2,Rlx,[T2],N1).
+	%pcmj(L1,Lx,0).
+
+testarmj(L1,L2,Lx,[2|T2],N):-subs(N,Lx,Rlx),
+	N1 is N + 1,
+	testarmj(L1,L2,Rlx,[T2],N1).
+	%pcmj(L1,Lx,0).
+
+testarmj(L1,L2,Lx,[H|T2],N):-H \== 1,
+	H \== 2,
+	N1 is N + 1,
+	testarmj(L1,L2,Lx,T2,N1).
+
+testarmj(L1,_,Lx,[],_):-!,
+	pcmj(L1,Lx).
+
+subs(1,[_|T],[0|T]).
+
+subs(N,[H|T],[H|Lx]):-N > 1,
+	N1 is N - 1,
+	subs(N1,T,Lx).
+
+%pesquisa2(L1,[H|T],[]):-subsetCounter(L1,H,0,HListax),
+	%print(Listax),nl,
+	%pcWin(L1,Listax,0),
+%	(pesquisa2(L1,T,[HListax])).
+
+%pcmj(_,[],_).
+
+pcmj(L1,Lx):-max(Lx,Max),
+	pcmj1(L1,Lx,Max,0).
+
+pcmj1(L1,[H|T],Max,N):-H \== Max,
+	N1 is N + 1,
+	pcmj1(L1,T,Max,N1).
+
+pcmj1(L1,[Max|_],Max,N):-N1 is N + 1,
+	findall(A,(vitoriax(A)),Lv),
+	pcmj2(L1,Lv,N1).
+
+pcmj2(L1,[H|_],1):-pcmj3(L1,H).
+
+pcmj2(L1,[_|T],N):-N \== 1,
+	N1 is N - 1,
+	pcmj2(L1,T,N1).
+
+pcmj3([],[H|_]):-tell('MelhorJogada.txt'),
+	write('PcProb#'),
+	write(H),
+	nl,
+	told.
+
+pcmj3([H|T],[H2|T2]):-H \== H2,
+	pcmj3(T,[H2|T2]).
+
+pcmj3([H|T],[H|B]):-pcmj3(T,B).
+
+max([X],X).
+
+max([X|Xs],X):- max(Xs,Y), X >=Y.
+
+max([X|Xs],N):- max(Xs,N), N > X.
 
 
 %testes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
