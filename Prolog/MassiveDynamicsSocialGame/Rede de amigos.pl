8dc5134e57d348b62952b87907ae565045ea8f16
Marco Garcia <marcoamgarcia17@gmail.com>
2014-01-10 12:05:20 +0000

    1101012: Desenvolver predicados para calcular rede de amigos de um utilizador (ate nivel 3). #22

---
+++ Prolog/MassiveDynamicsSocialGame/Rede de amigos.pl
@@ -0,0 +1,33 @@
+%base de conhecimento que depois e substituida por BdD
+amizade(marco,joao).
+amizade(marco,fabio).
+amizade(joao,miguel).
+amizade(joao,joel).
+amizade(joao,fabio).
+amizade(joao,marco).
+amizade(miguel,cristiano).
+amizade(miguel,maria).
+amizade(miguel,marco).
+
+%saber a rede de amigos de um utilizador
+amigosUtilizador(Ut,Lamif):-amigos1grau(Ut,Lami),amigos2grau(Ut,Lami,Lami,Lamif).
+
+amigos1grau(Ut,Lami):-findall(Ami,amizade(Ut,Ami),Lami).
+
+amigos2grau(Ut,[H|T],Laux,Lamif):-findall(Ami,amizade(H,Ami),Lami),
+	append(Lami,Laux,L),
+	amigos2grau(Ut,T,L,Lamif).
+
+amigos2grau(Ut,[],Laux,Lamif):-delete(Laux,Ut,Laux2),
+	set(Laux2,Lamif).
+
+%para eliminar duplicados
+set([],[]).
+
+set([H|T],[H|Out]) :-
+    \+(member(H,T)),
+    set(T,Out).
+
+set([H|T],Out) :-
+    member(H,T),
+    set(T,Out).
